2479170bc53a33f70ca229d8f2055a2cb6fe031a
IvanKulakov <84179036+IvanKulakov@users.noreply.github.com>
2024-11-19 02:03:58 +0200

    done

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
+//fancybox start
 try{
     Fancybox.bind("[data-fancybox]", {
         Images: {
@@ -11,3 +12,42 @@ try{
 catch (e){
     console.log(e)
 }
+//fancybox end
+
+//btn-up block start
+try{
+    const btnUp = document.getElementById('button-up');
+    const scrollUp = () => {
+        if (document.documentElement.scrollTop > 300) {
+            btnUp.classList.remove('show');
+        } else {
+            btnUp.classList.add('show')
+        }
+    }
+
+    const scrollingUp = () => {
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        });
+    }
+
+    btnUp.addEventListener('click', scrollingUp );
+    window.addEventListener('scroll', scrollUp);
+}
+catch (e){
+    console.log(e)
+}
+//btn-up block end
+
+const slider2 = document.querySelector('.swiper');
+const sl2 = new Swiper(slider2,{
+    direction: 'horizontal',
+    loop: false,
+    speed: 500,
+    navigation: {
+        nextEl: ".nav-next",
+        prevEl: ".nav-prev",
+    },
+})
